b5a944e1021791465cb5f67535bb5ca98ee8cd6c
jakobryd1 <jakob.rydberg0201@gmail.com>
2024-04-19 13:37:24 +0200

    Merged the two branches

--- Website/MapCreator.py
+++ Website/MapCreator.py
@@ -1,20 +1,24 @@
 import plotly.express as px
 import pandas as pd
+from Website.Database.DatabaseConnector import *
+
 
 def getMap():
     df: pd.DataFrame = setUpData()
     return createMap(df)
-    #fig.show()
 
 
 def setUpData() -> pd.DataFrame:
     print("Getting data")
+
+    fetcher = DatabaseConnector()
+    data = fetcher.getData()
     # Just some placeholder data for now
-    data: dict = {
-    "idnr": [1, 2, 3],
-    "lon": [12.45, 12.48, 12.43],
-    "lat": [41.88, 41.89, 41.8]
-    }
+    # data: dict = {
+    # "idnr": [1, 2, 3],
+    # "lon": [12.45, 12.48, 12.43],
+    # "lat": [41.88, 41.89, 41.8]
+    # }
     return pd.DataFrame(data)
 
 def createMap(df : pd.DataFrame) -> px.scatter_mapbox:
@@ -29,3 +33,7 @@ def createMap(df : pd.DataFrame) -> px.scatter_mapbox:
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":50, "l":0, "b":10})
     return fig
+
+if __name__ == "__main__":
+    fig = getMap()
+    fig.show()
