e473336d934571db154bafa189a13309f13fa997
ErikJunkers <117827955+ErikJunkers@users.noreply.github.com>
2024-05-09 14:54:01 +0200

    Uppdaterad så att csv filen med städer inte används

--- Website/website.py
+++ Website/website.py
@@ -2,9 +2,12 @@ from flask import Flask, redirect, url_for, render_template, request, flash
 import plotly.express as px
 from MapCreator import getMap
 from CityToCoordinates import city2Coordinates
+from geopy.geocoders import Nominatim
+from functools import partial
 
 app = Flask(__name__)
 app.secret_key = "AsdsjahdHkNFhade134vksln"
+geolocator = Nominatim(user_agent="my_app_name")
 
 @app.route("/")
 def index():
@@ -26,14 +29,17 @@ def search():
 
 
 def searchWithCityHelper(city):
+    geocode = partial(geolocator.geocode, language="es")
     if request.method == "POST":
         print(city)
         try:
-            coordinates = city2Coordinates(city, "Website/static/worldcities.csv")
-            lt = coordinates[0]
-            ln = coordinates[1]
-            print(ln)
-            print(lt)
+            # coordinates = city2Coordinates(city, "Website/static/worldcities.csv")
+            # lt = coordinates[0]
+            # ln = coordinates[1]
+            # print(ln)
+            # print(lt)
+            lt = geocode(city).latitude
+            ln = geocode(city).longitude
             fig = getMap()
             fig.update_layout(mapbox=dict(center=dict(lat=lt, lon=ln), zoom=8))
             div = fig.to_html(full_html=False)
