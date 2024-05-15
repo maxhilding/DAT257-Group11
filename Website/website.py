e67c569ff0855962f92a229877bf89a6a4125669
Marcus-Wassenius <144217820+Marcus-Wassenius@users.noreply.github.com>
2024-05-15 10:23:14 +0200

    Update website.py

--- Website/website.py
+++ Website/website.py
@@ -10,18 +10,12 @@ geolocator = Nominatim(user_agent="my_app_name")
 
 @app.route("/",  methods=["POST", "GET"])
 def index():
-    geocode = partial(geolocator.geocode, language="es")
+    
     if request.method == "POST":
         search = request.form["search"]
         print(search)
         try:
-            if "," in search:
-                l = search.split(",")
-                lt = float(l[0])
-                ln = float(l[1])
-            else:
-                lt = geocode(search).latitude
-                ln = geocode(search).longitude
+            lt, ln = search_helper(search)
             fig = getMap()
             fig.update_layout(mapbox=dict(center=dict(lat=lt, lon=ln), zoom=8))
             div = fig.to_html(full_html=False)
@@ -43,6 +37,19 @@ def about():
 @app.route("/home")
 def home():
     return redirect(url_for("index"))
+
+
+def search_helper(search):
+    geocode = partial(geolocator.geocode, language="es")
+    if "," in search:
+        l = search.split(",")
+        lt = float(l[0])
+        ln = float(l[1])
+    else:
+        lt = geocode(search).latitude
+        ln = geocode(search).longitude
+    return lt, ln
+
 
 if __name__ == "__main__":
     app.run(debug=True)
